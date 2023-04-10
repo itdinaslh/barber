572f0772d59d3e35ad214a57a2a70d98806fe140
itdinaslh <104603982+itdinaslh@users.noreply.github.com>
2023-04-10 09:23:56 +0700

    Update Logout

--- routes/api.php
+++ routes/api.php
@@ -24,3 +24,7 @@ Route::get('/test', function() {
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth:sanctum')->group(function() {
+    Route::post('/logout', [UserApiController::class, 'logout']);
+});
