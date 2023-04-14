5b3603e6e0e85a45a7caf11b24941092277f4d01
itdinaslh <itdlh@outlook.com>
2023-04-15 02:19:20 +0700

    Add Cost Api

---
+++ app/Http/Controllers/CostApiController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use DB;
+use Illuminate\Http\Request;
+use App\Models\CostOp;
+use App\Models\Operational;
+use DateTime;
+use Carbon\Carbon;
+
+class CostApiController extends Controller
+{
+    public function monthly() {
+        $curDate = new DateTime(date('Y-m-d'));
+        $firstDate = $curDate->format('Y-m-01');
+        $lastDate = $curDate->format('Y-m-t');
+
+        $data = DB::table('cost_op as co')
+            ->join('operational as o', 'co.opID', 'o.id')
+            ->select('co.id', 'o.NamaOp', 'co.Total', 'co.created_by',
+                DB::raw('DATE_FORMAT(co.Tanggal, "%d-%m-%Y") as Tanggal'),
+                DB::raw("DATE_FORMAT(co.created_at, '%H:%i:%s') as Time")
+            )
+            ->whereBetween('co.Tanggal', [$firstDate, $lastDate])
+            ->get();
+
+        if (!is_null($data)) {
+            foreach($data as $row) {
+                $row->Total = number_format($row->Total, 0, ',', '.');
+            }
+        }
+
+        return response()->json($data, 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserApiController;
 use App\Http\Controllers\BarberApiController;
 use App\Http\Controllers\TransApiController;
+use App\Http\Controllers\CostApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,6 +30,7 @@ Route::get('/barberman', [BarberApiController::class, 'index']);
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+Route::get('/cost/thismonth', [CostApiController::class, 'monthly']);
 
 Route::middleware('auth:sanctum')->group(function() {
     Route::get('/user', [UserApiController::class, 'index']);
