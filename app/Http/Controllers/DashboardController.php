3202e5a7800be3b5727982e3aa74873f1b3e09c6
itdinaslh <104603982+itdinaslh@users.noreply.github.com>
2023-06-18 21:45:48 +0700

    update euy

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Transaction;
 
 class DashboardController extends Controller
 {
     //
     public function index() {
-        return view('dashboard.index');
+        $currentMonth = date('m');
+        $today = Transaction::whereDate('created_at', now()->today())->count();
+        $todaytotal = Transaction::whereDate('created_at', now()->today())->sum('TotalPaid');
+        $week = Transaction::whereBetween('created_at', [now()->startOfWeek(), now()->endOfWeek()])->count();
+        $weektotal = Transaction::whereBetween('created_at', [now()->startOfWeek(), now()->endOfWeek()])->sum('TotalPaid');
+        $month = Transaction::whereMonth('created_at', $currentMonth)->count();
+        $monthtotal = Transaction::whereMonth('created_at', $currentMonth)->sum('TotalPaid');
+        return view('dashboard.index', ['today'=> $today, 'week'=>$week, 'month'=>$month,
+        'todaytotal'=> $todaytotal, 'weektotal'=>$weektotal, 'monthtotal'=>$monthtotal
+    ]);
     }
 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -7,13 +7,33 @@
 
 @section('content')
     <div class="row">
-        <div class="col-xs-12">
-            <div class="box">
-                <div class="box-body">
-                    <div>
-                        <img src="{{ asset('/img/bg-mb.png') }}" style="margin-left:auto;margin-right:auto;"
-                            class="img img-responsive" alt="logo">
-                    </div>
+        <div class="col-md-4">
+            <div class="info-box bg-yellow">
+                <span class="info-box-icon"><i class="ion ion-ios-pricetag-outline"></i></span>
+                <div class="info-box-content">
+                    <span class="info-box-text">Total Hari ini</span>
+                    <span class="info-box-number">{{ $today }}</span>
+                    <span class="info-box-number">Rp. {{ number_format($todaytotal, 0, ',', '.') }}</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-xs-4">
+            <div class="info-box bg-aqua">
+                <span class="info-box-icon"><i class="ion ion-ios-pricetag-outline"></i></span>
+                <div class="info-box-content">
+                    <span class="info-box-text">Total Minggu ini</span>
+                    <span class="info-box-number">{{ $week }}</span>
+                    <span class="info-box-number">Rp. {{ number_format($todaytotal, 0, ',', '.') }}</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-xs-4">
+            <div class="info-box bg-red">
+                <span class="info-box-icon"><i class="ion ion-ios-pricetag-outline"></i></span>
+                <div class="info-box-content">
+                    <span class="info-box-text">Total Bulan ini</span>
+                    <span class="info-box-number">{{ $month }}</span>
+                    <span class="info-box-number">Rp. {{ number_format($monthtotal, 0, ',', '.') }}</span>
                 </div>
             </div>
         </div>
